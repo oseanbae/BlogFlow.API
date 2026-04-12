0bd3221b517ac0a4c527544774ece65557e99711
John Paul Atencio <johnpaultnc26@gmail.com>
2026-04-13 00:56:48 +0800

    JsonConverter(typeof(JsonStringEnumConverter))]

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace BlogFlow.API.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum UserRole
     {
         Admin = 1,
@@ -15,13 +16,17 @@ namespace BlogFlow.API.Models
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
 
-        [Required, StringLength(50, MinimumLength = 3)]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public required string Username { get; set; }
 
-        [Required, EmailAddress, StringLength(100)]
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
         public required string Email { get; set; }
 
-        [Required, StringLength(255, MinimumLength = 6)]
+        [Required]
+        [StringLength(255, MinimumLength = 6)]
         public required string PasswordHash { get; set; }
 
         [Required]
@@ -29,10 +34,13 @@ namespace BlogFlow.API.Models
 
         [Required]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
         [Required]
         public bool IsDeleted { get; set; } = false;
+
         public DateTime? DeletedAt { get; set; }
 
+        // Navigation property
         public ICollection<RefreshToken> RefreshTokens { get; set; } = [];
     }
 }
