742d8978550cfc17b43e0ce5d2e180ec14d5bc4d
John Paul Atencio <johnpaultnc26@gmail.com>
2026-04-12 10:49:59 +0800

    feat: migrated latest refresh token

---
+++ Migrations/20260412024858_UpdatedRefreshToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogFlow.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReplacedByTokenId",
+                table: "RefreshTokens",
+                newName: "ReplacedByToken");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReplacedByToken",
+                table: "RefreshTokens",
+                newName: "ReplacedByTokenId");
+        }
+    }
+}
