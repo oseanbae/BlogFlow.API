4f483065873d6b0ecf94fd10e7bb569a60d98a1a
John Paul Atencio <johnpaultnc26@gmail.com>
2026-05-22 21:45:30 +0800

    refactor(exception): add constructor overload to NotFoundException to handle collections of IDs

--- Exceptions/NotFoundException.cs
+++ Exceptions/NotFoundException.cs
@@ -5,16 +5,21 @@ namespace BlogFlow.API.Exceptions
     public sealed class NotFoundException : AppException
     {
         public string EntityName { get; }
-        public Guid EntityId { get; }
+        public IEnumerable<Guid> EntityIds { get; }
 
         public NotFoundException(string entityName, Guid entityId)
-            : base (
-                message: $"{entityName} with ID {entityId} was not found",
-                errorCode: $"{entityName.Replace(" ","_").ToUpper()}_NOT_FOUND",
-                statusCode: StatusCodes.Status404NotFound)
+            : this(entityName, [entityId]) { } 
+
+        public NotFoundException(string entityName, IEnumerable<Guid> entityIds)
+            : base(
+                message: entityIds.Count() == 1
+                ? $"{entityName} with ID {entityIds.First()} was not found"
+                : $"{entityName} with IDs ({string.Join(", ", entityIds)}) were not found",
+            errorCode: $"{entityName.Replace(" ", "_").ToUpper()}_NOT_FOUND",
+            statusCode: StatusCodes.Status404NotFound)
         {
             EntityName = entityName;
-            EntityId = entityId;
+            EntityIds = entityIds;
         }
     }
 }
